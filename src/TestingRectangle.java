28106844b33a38e1e3fc8f2796a5fd9bbb4d2f60
zonesama <trannhatthanh31@gmail.com>
2019-10-23 09:46:33 +0700

    First Commit.

--- src/TestingRectangle.java
+++ src/TestingRectangle.java
@@ -13,13 +13,13 @@ public class TestingRectangle {
                 System.out.print("Enter the height:");
                 height = sc.nextDouble();
             } while (height <= 0);
+            Rectangle rectangle = new Rectangle(width, height);
+            System.out.println("Your Rectangle \n"+ rectangle.display());
+            System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+            System.out.println("Area of the Rectangle: "+ rectangle.getArea());
         } catch (Exception e) {
             System.err.println(e);
         }
 
-        Rectangle rectangle = new Rectangle(width, height);
-        System.out.println("Your Rectangle \n"+ rectangle.display());
-        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
-        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
     }
 }
